6981ac2c45694c994a808d45f02ad292b7d21189
JoshuaJDevine <jd8386@gmail.com>
2021-03-03 14:08:00 -0800

    need to do recursive

--- problems/graph.js
+++ problems/graph.js
@@ -53,6 +53,7 @@ class Graph {
               this.adjList[currentVertex].forEach(neighbor => {
                 if(!visited.has(neighbor)){
                     visited.add(neighbor);
+                    // console.log("ADDING ", currentVertex, " to visited");
                     queue.push(neighbor);
                 }
               });
@@ -61,16 +62,91 @@ class Graph {
       return result;
   }
 
-  depthFirstTraversalIterative(startingVertex) {
-    // Code goes here ...
-  }
+  // ['a', ['b', 'c', 'd']]
+  // ['d', ['g']]
+  // ['b', ['c', 'e']]
+  // ['c', ['f', 'g']]
+  // ['f', ['g']]
+  // ['h']
+  //'a', 'b', 'c', 'd', 'e', 'f', 'g'
 
+  depthFirstTraversalIterative(startingVertex) {
+    // let result = [];
+    // let visited = new Set();
+    // visited.add(startingVertex);
+    // result.push(startingVertex);
+    // for(let i = this.adjList[startingVertex].length -1; i > 0; i--){
+    //   let neighbor = this.adjList[i];
+    //   // visited.add(neighbor);
+    //     // if (!result.includes(neighbor)){
+    //     //   result.push(neighbor);
+    //     // }
+    //     for(let j = this.adjList[neighbor].length -1; j > 0; j--){
+    //       console.log(this.adjList[neighbor][j])
+    //       // visited.add(neighbor[i]);
+    //       // if (!result.includes(neighbor[i])){
+    //       //   result.push(this.adjList[neighbor[i]]);
+    //       // }
+    //     }
+    //     // this.adjList[neighbor].forEach (neighbor =>{
+    //     //   visited.add(neighbor);
+    //     //   if (!result.includes(neighbor)){
+    //     //     result.push(neighbor);
+    //     //   }
+    //     // })
+    //   }
+    // return result;
+
+
+
+
+    let queue = [startingVertex];
+    let result = [];
+    let visited = new Set();
+
+    while(queue.length >= 1){
+        let currentVertex = queue.pop();
+        result.push(currentVertex);
+        visited.add(currentVertex);
+        if(this.adjList[currentVertex]){
+            this.adjList[currentVertex].forEach(neighbor => {
+              if(!visited.has(neighbor)){
+                  visited.add(neighbor);
+                  // console.log("ADDING ", currentVertex, " to visited");
+                  queue.push(neighbor);
+              }
+            });
+        }
+    }
+    return result;
+  }
   depthFirstTraversalRecursive(startingVertex, visited = new Set(), vertices = []) {
     // Code goes here ...
   }
 
 }
 
+// const edges =
+// [['a', 'b'],
+// ['a', 'c'],
+// ['a', 'd'],
+// ['d', 'g'],
+// ['b', 'c'],
+// ['b', 'e'],
+// ['c', 'f'],
+// ['c', 'g'],
+// ['f', 'g'],
+// ['h']]
+// let testG = new Graph();
+// for (let edge of edges) {
+//   if (edge.length === 1) {
+//     testG.addVertex(edge[0])
+//   } else {
+//     testG.addEdges(edge[0], edge[1])
+//   }
+// }
+
+// testG.breadthFirstTraversal("a");
 // let testGraph = new Graph();
 // testGraph.addEdges('f', 'a')
 // //expect { 'f': ['a'], 'a': ['f'] }
